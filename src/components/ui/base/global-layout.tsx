18488eddbe3aca3124522c51667033d14240b701
ungheung <t01047377477@gmail.com>
2026-02-03 00:03:37 +0900

    feat : kbd 추가

--- src/components/ui/base/global-layout.tsx
+++ src/components/ui/base/global-layout.tsx
@@ -1,4 +1,5 @@
 import { Outlet } from "react-router";
+import { Toaster } from "sonner";
 
 export default function GlobalLayout() {
   return (
@@ -9,6 +10,7 @@ export default function GlobalLayout() {
       <main className="max-h-[calc(100vh-var(--spacing)*20.5)]">
         <Outlet />
       </main>
+      <Toaster />
     </div>
   );
 }
