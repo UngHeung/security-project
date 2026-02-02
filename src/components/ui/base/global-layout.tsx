35e296a592ed17ba2b935498bb1bb9e0f8174123
ungheung <t01047377477@gmail.com>
2026-02-02 18:55:34 +0900

    style : main 높이 설정

--- src/components/ui/base/global-layout.tsx
+++ src/components/ui/base/global-layout.tsx
@@ -2,14 +2,13 @@ import { Outlet } from "react-router";
 
 export default function GlobalLayout() {
   return (
-    <>
-      <header>
-        <h2>보안가이드 프로젝트</h2>
+    <div className="mx-auto max-w-lg p-4">
+      <header className="h-12.5">
+        <h2 className="text-center">보안가이드 프로젝트</h2>
       </header>
-      <main>
+      <main className="max-h-[calc(100vh-var(--spacing)*20.5)]">
         <Outlet />
       </main>
-      ;
-    </>
+    </div>
   );
 }
