609a629f58fab78b4799a682bd82654bf45b1e63
ungheung <t01047377477@gmail.com>
2026-02-02 21:37:36 +0900

    feat : 가이드 상세 페이지 추가

---
+++ src/pages/guide-detail.tsx
@@ -0,0 +1,95 @@
+import { Badge } from "@/components/ui/badge";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+} from "@/components/ui/carousel";
+import { TagsIcon } from "hugeicons-react";
+
+// 임시 값
+const title = "제목입니다. 긴 제목은 줄이 바뀝니다. 바뀌는지 확인해볼까요?";
+const writer = "홍길동";
+const position = "파트장";
+const images = [
+  "https://picsum.photos/800/600",
+  "https://picsum.photos/800/600",
+  "https://picsum.photos/800/600",
+];
+const content = `내용입니다.
+내용입니다.
+내용이에요.`;
+const tags = ["반출", "정보자산", "노트북"];
+const locations = ["자산", "정보자산"];
+
+export default function GuideDetailPage() {
+  return (
+    <div>
+      <header className="flex flex-col items-start gap-2 border-b py-2">
+        <h2 className="font-semibold">{title}</h2>
+        <div className="flex w-full items-center justify-end gap-1 text-sm">
+          <span className="font-semibold">{writer}</span>
+          <span>{position}</span>
+          <span className="bg-accent h-7 w-7 rounded-full"></span>
+        </div>
+      </header>
+
+      <ul className="mt-2 flex items-center gap-0.5 px-1 text-sm">
+        <li>
+          <span className="bg-muted-foreground text-muted mr-1.5 rounded-lg border-0 px-1 py-0.5">
+            위치
+          </span>
+        </li>
+        {locations.map((location, index) => {
+          if (index < locations.length - 1) {
+            return (
+              <li>
+                <span className="bg-muted rounded-lg px-1 py-0.5">
+                  {location}
+                </span>
+                <span className="mx-1 text-xs">{">"}</span>
+              </li>
+            );
+          } else {
+            return (
+              <li>
+                <span className="bg-muted rounded-lg px-1 py-0.5">
+                  {location}
+                </span>
+              </li>
+            );
+          }
+        })}
+      </ul>
+
+      <div>
+        <Carousel>
+          <CarouselContent className="mt-3">
+            {images.map((image) => (
+              <CarouselItem className="basis-8/9">
+                <img
+                  className="rounded-lg"
+                  src="https://picsum.photos/800/600"
+                />
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+        </Carousel>
+      </div>
+
+      <div className="flex flex-col">
+        <p className="border-b border-dashed p-4 text-base whitespace-pre-wrap">
+          {content}
+        </p>
+
+        <ul className="mt-2 flex items-center justify-end gap-1">
+          {tags.map((tag, index) => (
+            <Badge key={index} variant={"secondary"} className="cursor-default">
+              {tag}
+            </Badge>
+          ))}
+          <TagsIcon className="text-muted-foreground h-5 w-5" />
+        </ul>
+      </div>
+    </div>
+  );
+}
--- src/root-router.tsx
+++ src/root-router.tsx
@@ -2,13 +2,17 @@ import { Navigate, Route, Routes } from "react-router";
 import GlobalLayout from "./components/ui/base/global-layout";
 import DashboardPage from "./pages/dashboard";
 import SearchPage from "./pages/search";
+import GuideDetailPage from "./pages/guide-detail";
 
 export default function RootRouter() {
   return (
     <Routes>
       <Route element={<GlobalLayout />}>
         <Route path="/" element={<DashboardPage />} />
+
         <Route path="/search" element={<SearchPage />} />
+
+        <Route path="/guide/:id" element={<GuideDetailPage />} />
       </Route>
 
       <Route path="*" element={<Navigate to={"/"} />} />
