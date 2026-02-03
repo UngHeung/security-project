f64c723953dbda59f518aeb76c66f20dd04d0aa6
ungheung <t01047377477@gmail.com>
2026-02-04 06:13:58 +0900

    feat : 메뉴, 프로필버튼 추가

--- src/components/ui/base/global-layout.tsx
+++ src/components/ui/base/global-layout.tsx
@@ -1,13 +1,22 @@
+import { Button } from "@/components/ui/button";
 import { Outlet } from "react-router";
 import { Toaster } from "sonner";
+import Menu from "./menu";
+import ProfileButton from "./profile-button";
 
 export default function GlobalLayout() {
   return (
-    <div className="mx-auto max-w-lg p-4">
-      <header className="h-12.5">
-        <h2 className="text-center">보안가이드 프로젝트</h2>
+    <div className="mx-auto max-w-lg px-4">
+      <header className="flex h-12.5 items-center justify-between">
+        <h2 className="basis-1/5">보안가이드</h2>
+        <div className="basis-3/5">
+          <Menu />
+        </div>
+        <div className="flex basis-1/5 gap-1">
+          <ProfileButton size={20} />
+        </div>
       </header>
-      <main className="max-h-[calc(100vh-var(--spacing)*20.5)]">
+      <main className="max-h-[calc(100vh-var(--spacing)*12.5)]">
         <Outlet />
       </main>
       <Toaster />
---
+++ src/components/ui/base/menu.tsx
@@ -0,0 +1,76 @@
+import { Button } from "@/components/ui/button";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+} from "@/components/ui/navigation-menu";
+import { useNavigate } from "react-router";
+
+export default function Menu() {
+  const navigate = useNavigate();
+  return (
+    <div>
+      <NavigationMenu>
+        <NavigationMenuList className="gap-2">
+          <NavigationMenuItem>
+            <NavigationMenuLink>
+              <Button variant="ghost" onClick={() => navigate("/")}>
+                메인
+              </Button>
+            </NavigationMenuLink>
+          </NavigationMenuItem>
+
+          <NavigationMenuItem>
+            <NavigationMenuLink>
+              <Button variant="ghost" onClick={() => navigate("/search")}>
+                검색
+              </Button>
+            </NavigationMenuLink>
+          </NavigationMenuItem>
+
+          <NavigationMenuItem>
+            <NavigationMenuTrigger className="cursor-pointer">
+              게시판
+            </NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <ul className="flex flex-col">
+                <li>
+                  <Button
+                    variant="ghost"
+                    onClick={() => navigate("/guide/write")}
+                  >
+                    가이드작성
+                  </Button>
+                </li>
+                <li>
+                  <Button
+                    variant="ghost"
+                    onClick={() => navigate("/guide/update")}
+                  >
+                    가이드수정
+                  </Button>
+                </li>
+                <li>
+                  <Button variant="ghost" onClick={() => navigate("/guide/1")}>
+                    가이드샘플
+                  </Button>
+                </li>
+                <li>
+                  <Button
+                    variant="ghost"
+                    onClick={() => navigate("/board/notice")}
+                  >
+                    공지사항
+                  </Button>
+                </li>
+              </ul>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+    </div>
+  );
+}
