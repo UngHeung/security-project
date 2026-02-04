6b8b372170554252d2671102eee718a21ab0b76d
ungheung <t01047377477@gmail.com>
2026-02-04 22:22:19 +0900

    feat : 로그인상태 및 로그인메뉴 표시 컴포넌트

---
+++ src/components/ui/base/auth-status.tsx
@@ -0,0 +1,66 @@
+import defaultAvatar from "@/assets/default-avatar.jpg";
+import ProfileButton from "@/components/ui/base/profile-button";
+import { Button } from "@/components/ui/button";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { useState } from "react";
+import { useNavigate } from "react-router";
+
+// 임시
+
+export default function AuthStatus() {
+  const navigate = useNavigate();
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  return (
+    <>
+      <Popover>
+        <PopoverTrigger>
+          <ProfileButton size={20} avatarUrl={defaultAvatar} />
+        </PopoverTrigger>
+        <PopoverContent className="max-w-20">
+          <ul className="flex flex-col items-start gap-2">
+            {isLoggedIn ? (
+              <>
+                <li>
+                  <Button variant={"ghost"}>프로필</Button>
+                </li>
+                <li>
+                  <Button
+                    variant={"ghost"}
+                    onClick={() => setIsLoggedIn(false)}
+                  >
+                    로그아웃
+                  </Button>
+                </li>
+              </>
+            ) : (
+              <>
+                <li>
+                  <Button
+                    variant={"ghost"}
+                    onClick={() => navigate("/sign-in")}
+                  >
+                    로그인
+                  </Button>
+                </li>
+                <li>
+                  <Button
+                    variant={"ghost"}
+                    onClick={() => navigate("/sign-up")}
+                  >
+                    회원가입
+                  </Button>
+                </li>
+              </>
+            )}
+          </ul>
+        </PopoverContent>
+      </Popover>
+    </>
+  );
+}
