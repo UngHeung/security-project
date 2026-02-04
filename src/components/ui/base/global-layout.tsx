4e3c8a20c78fbc842f5518480b81f3245346cb78
ungheung <t01047377477@gmail.com>
2026-02-04 22:23:41 +0900

    style : 상단 스타일 변경

--- src/components/ui/base/global-layout.tsx
+++ src/components/ui/base/global-layout.tsx
@@ -1,22 +1,21 @@
-import { Button } from "@/components/ui/button";
 import { Outlet } from "react-router";
 import { Toaster } from "sonner";
+import AuthStatus from "./auth-status";
 import Menu from "./menu";
-import ProfileButton from "./profile-button";
 
 export default function GlobalLayout() {
   return (
     <div className="mx-auto max-w-lg px-4">
       <header className="flex h-12.5 items-center justify-between">
-        <h2 className="basis-1/5">보안가이드</h2>
-        <div className="basis-3/5">
+        <h2 className="basis-2/6">보안가이드</h2>
+        <div className="basis-3/6">
           <Menu />
         </div>
-        <div className="flex basis-1/5 gap-1">
-          <ProfileButton size={20} />
+        <div className="flex basis-1/6 justify-center gap-1">
+          <AuthStatus />
         </div>
       </header>
-      <main className="max-h-[calc(100vh-var(--spacing)*12.5)]">
+      <main className="max-h-[calc(100vh-var(--spacing)*16.5)] pt-4">
         <Outlet />
       </main>
       <Toaster />
