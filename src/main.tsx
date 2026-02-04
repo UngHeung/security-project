073671f2fe7ebb92aa9b95e5f5c019c319e94a98
ungheung <t01047377477@gmail.com>
2026-02-05 05:49:57 +0900

    update : tanstack queryclient 적용

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,23 @@
 import { createRoot } from "react-dom/client";
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter } from "react-router";
 import App from "./App.tsx";
 import "./index.css";
-import { BrowserRouter } from "react-router";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </BrowserRouter>,
 );
