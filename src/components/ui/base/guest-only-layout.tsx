e9156e2eb1f07400870cbd323538af9256cb1589
ungheung <t01047377477@gmail.com>
2026-02-06 23:57:49 +0900

    feat : 유저, 비유저 접근 분리

---
+++ src/components/ui/base/guest-only-layout.tsx
@@ -0,0 +1,10 @@
+import { useSession } from "@/store/session";
+import { Navigate, Outlet, useNavigate } from "react-router";
+
+export default function GuestOnlyLayout() {
+  const session = useSession();
+
+  if (session) return <Navigate to={"/"} replace />;
+
+  return <Outlet />;
+}
--- src/root-router.tsx
+++ src/root-router.tsx
@@ -1,5 +1,7 @@
 import { Navigate, Route, Routes } from "react-router";
 import GlobalLayout from "./components/ui/base/global-layout";
+import GuestOnlyLayout from "./components/ui/base/guest-only-layout";
+import MemberOnlyLayout from "./components/ui/base/member-only-layout";
 import ForgetPasswordPage from "./pages/auth/forget-password";
 import ResetPasswordPage from "./pages/auth/reset-password";
 import SignInPage from "./pages/auth/sign-in";
@@ -13,18 +15,35 @@ export default function RootRouter() {
   return (
     <Routes>
       <Route element={<GlobalLayout />}>
+        <Route element={<GuestOnlyLayout />}>
+          {/* auth */}
+          <Route path="/sign-up" element={<SignUpPage />} />
+          <Route path="/sign-in" element={<SignInPage />} />
+          <Route path="/forget-password" element={<ForgetPasswordPage />} />
+        </Route>
+
         <Route path="/" element={<DashboardPage />} />
 
-        <Route path="/sign-up" element={<SignUpPage />} />
-        <Route path="/sign-in" element={<SignInPage />} />
-        <Route path="/forget-password" element={<ForgetPasswordPage />} />
-        <Route path="/reset-password" element={<ResetPasswordPage />} />
-
+        {/* search */}
         <Route path="/search" element={<SearchPage />} />
 
+        {/* guide */}
         <Route path="/guide/:id" element={<GuideDetailPage />} />
-        <Route path="/guide/write" element={<GuideEditPage type="create" />} />
-        <Route path="/guide/update" element={<GuideEditPage type="update" />} />
+
+        <Route element={<MemberOnlyLayout />}>
+          {/* auth */}
+          <Route path="/reset-password" element={<ResetPasswordPage />} />
+
+          {/* guide */}
+          <Route
+            path="/guide/write"
+            element={<GuideEditPage type="create" />}
+          />
+          <Route
+            path="/guide/update"
+            element={<GuideEditPage type="update" />}
+          />
+        </Route>
       </Route>
 
       <Route path="*" element={<Navigate to={"/"} />} />
