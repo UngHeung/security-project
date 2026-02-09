3164ded03c3878ec12730885272327cddf496be2
ungheung <t01047377477@gmail.com>
2026-02-10 06:28:35 +0900

    rename : 파일명 변경

---
+++ src/components/ui/base/profile-image.tsx
@@ -0,0 +1,23 @@
+export default function ProfileImage({
+  size,
+  avatarUrl,
+}: {
+  size: number;
+  avatarUrl?: string;
+}) {
+  return (
+    <>
+      <div
+        className={`flex h-6 w-6 items-center justify-center overflow-hidden rounded-full border-none`}
+      >
+        <img
+          src={avatarUrl}
+          alt="My avatar"
+          width={size}
+          height={size}
+          className="rounded-full"
+        />
+      </div>
+    </>
+  );
+}
