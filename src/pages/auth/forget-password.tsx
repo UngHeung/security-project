91b1a5678b666127e95105640a2163848a4dbc61
ungheung <t01047377477@gmail.com>
2026-02-10 05:19:55 +0900

    feat : 비밀번호 재설정 기능

--- src/pages/auth/forget-password.tsx
+++ src/pages/auth/forget-password.tsx
@@ -1,23 +1,41 @@
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { useRequestPasswordResetEmail } from "@/hooks/mutations/use-request-password-reset-email";
+import { generateErrorMessage } from "@/lib/error";
+import { useRef, useState } from "react";
+import { useNavigate } from "react-router";
 import { toast } from "sonner";
 
-// 임시
-const myEmail = "beehive2838@naver.com";
-
 export default function ForgetPasswordPage() {
-  const { mutate, isPending } = useRequestPasswordResetEmail({
+  const navigate = useNavigate();
+
+  const [email, setEmail] = useState("");
+
+  const emailRef = useRef<HTMLInputElement>(null);
+
+  const {
+    mutate: requestPasswordResetEmail,
+    isPending: isRequestPasswordResetEmailPending,
+  } = useRequestPasswordResetEmail({
     onSuccess: () => {
       toast.info("인증메일 발송 완료", { position: "top-center" });
+      navigate("/sign-in");
     },
-    onError: () => {
-      toast.error("에러가 발생했습니다.", { position: "top-center" });
+    onError: (error) => {
+      const message = generateErrorMessage(error);
+      toast.error(message, { position: "top-center" });
     },
   });
 
   const handleSendEmail = () => {
-    mutate(myEmail);
+    if (!email.trim()) {
+      toast.error("이메일을 작성해주세요.", { position: "top-center" });
+      emailRef.current?.focus();
+      return;
+    }
+
+    requestPasswordResetEmail(email);
   };
 
   return (
@@ -26,12 +44,24 @@ export default function ForgetPasswordPage() {
         비밀번호 재설정
       </h3>
 
-      <Label className="text-muted-foreground">
+      <Label className="text-muted-foreground mb-2">
         등록된 이메일로 비밀번호 재설정 링크 발송
       </Label>
 
+      <Input
+        disabled={isRequestPasswordResetEmailPending}
+        type="email"
+        value={email}
+        ref={emailRef}
+        onChange={(event) => setEmail(event.target.value)}
+      />
+
       <div className="flex justify-end">
-        <Button disabled={isPending} className="mt-2" onClick={handleSendEmail}>
+        <Button
+          disabled={isRequestPasswordResetEmailPending}
+          className="mt-2"
+          onClick={handleSendEmail}
+        >
           링크 발송
         </Button>
       </div>
