5f1637756dd9e1aa1e50584045f015f533b68861
ungheung <t01047377477@gmail.com>
2026-02-11 06:26:19 +0900

    feat : 가이드 목록, 아이템

---
+++ src/pages/post/guide-list.tsx
@@ -0,0 +1,57 @@
+import FallBack from "@/components/fallback";
+import Loader from "@/components/loader";
+import { Button } from "@/components/ui/button";
+import { useInfiniteGuideData } from "@/hooks/queries/use-infinity-guides";
+import { useSession } from "@/store/session";
+import { useEffect } from "react";
+import { useInView } from "react-intersection-observer";
+import { useNavigate } from "react-router";
+import GuideItem from "./guide-item";
+
+export default function GuideList() {
+  const session = useSession();
+  const userId = session?.user.id;
+  const navigate = useNavigate();
+
+  const { data, error, isPending, fetchNextPage, isFetchingNextPage } =
+    useInfiniteGuideData();
+
+  const { ref, inView } = useInView();
+
+  useEffect(() => {
+    if (inView) {
+      fetchNextPage();
+    }
+  }, [inView]);
+
+  if (error) return <FallBack />;
+  if (isPending) return <Loader />;
+
+  return (
+    <div className="mt-4 flex flex-col gap-2">
+      {data.pages.length < 0 ? (
+        <div className="flex justify-center">
+          <span className="mt-4 text-sm">
+            작성된 가이드가 없습니다.{" "}
+            <Button
+              variant={"ghost"}
+              className="text-primary text-sm"
+              onClick={() => navigate("/guide/write")}
+            >
+              첫번째 가이드
+            </Button>
+            를 작성해주세요.
+          </span>
+        </div>
+      ) : (
+        data.pages.map((page) => {
+          return page.map((guideId, index) => {
+            return <GuideItem key={index} userId={userId} guideId={guideId} />;
+          });
+        })
+      )}
+      {isFetchingNextPage && <Loader />}
+      <div ref={ref}></div>
+    </div>
+  );
+}
