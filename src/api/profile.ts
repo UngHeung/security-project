60680f95dc46ad395ca56281d15ad9599516ac3e
ungheung <t01047377477@gmail.com>
2026-02-10 09:02:56 +0900

    feat : 프로필 생성, 변경 기능 추가

--- src/api/profile.ts
+++ src/api/profile.ts
@@ -1,4 +1,7 @@
+import { generateRandomName } from "@/lib/generate-random-name";
 import supabase from "@/lib/supabase";
+import { deleteImagesInPath, uploadImage } from "./image";
+import type { PositionType, RoleType } from "@/lib/types";
 
 export async function fetchProfile(userId: string) {
   const { data, error } = await supabase
@@ -12,31 +15,56 @@ export async function fetchProfile(userId: string) {
   return data;
 }
 
+export async function createProfile(userId: string) {
+  const randomName = generateRandomName();
+  const { data, error } = await supabase.from("profile").insert({
+    id: userId,
+    name: `_${randomName}`,
+  });
+
+  if (error) throw error;
+
+  return data;
+}
+
 export async function updateProfile({
   userId,
   name,
-  email,
-  phone,
-  avatar_url,
+  role,
+  avatarImageFile,
 }: {
   userId: string;
   name: string;
-  email: string;
-  phone: string;
-  avatar_url: string;
+  role: RoleType;
+  avatarImageFile?: File;
 }) {
+  // 기존 이미지 삭제
+  if (avatarImageFile) {
+    await deleteImagesInPath(`${userId}/avatar`);
+  }
+
+  let newAvatarImageUrl;
+  if (avatarImageFile) {
+    const fileExtension = avatarImageFile.name.split(".").pop() || "webp";
+    const filePath = `${userId}/avatar/${new Date().getTime()}-${crypto.randomUUID()}.${fileExtension}`;
+
+    newAvatarImageUrl = await uploadImage({
+      file: avatarImageFile,
+      filePath,
+    });
+  }
+
   const { data, error } = await supabase
     .from("profile")
     .update({
       name,
-      phone,
-      avatar_url,
-      email,
+      role,
+      avatar_url: newAvatarImageUrl,
     })
     .eq("id", userId)
+    .select()
     .single();
 
-  if (error) return error;
-
+  if (error) throw error;
   return data;
 }
