735ce3d29aa9953d0761d3c7907c3b31dc10c317
ungheung <t01047377477@gmail.com>
2026-02-14 20:49:45 +0900

    update : 글로벌로더 변경

--- src/components/global-loader.tsx
+++ src/components/global-loader.tsx
@@ -1,8 +1,13 @@
+import logo from "@/assets/logo.png";
+
 export const GlobalLoader = () => {
   return (
     <div className="bg-muted flex h-screen flex-col items-center justify-center">
       <div className="mb-15 flex animate-bounce items-center gap-4">
-        <div className="text-2xl font-bold">보안 가이드</div>
+        <div className="flex flex-col items-center justify-center text-2xl font-bold">
+          <img className="h-25 w-25" src={logo} alt="logo" />
+          <span className="-mt-5 cursor-default text-xl">Security Guide</span>
+        </div>
       </div>
     </div>
   );
