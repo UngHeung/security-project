3709aae6a6157f309459d1e5769396f43641ae06
ungheung <t01047377477@gmail.com>
2026-02-20 00:28:31 +0900

    style : 글로벌로더 수정

--- src/components/global-loader.tsx
+++ src/components/global-loader.tsx
@@ -3,10 +3,17 @@ import logo from "@/assets/logo.png";
 export const GlobalLoader = () => {
   return (
     <div className="bg-muted flex h-screen flex-col items-center justify-center">
-      <div className="mb-15 flex animate-bounce items-center gap-4">
+      <div className="mb-15 flex flex-col items-center gap-4">
         <div className="flex flex-col items-center justify-center text-2xl font-bold">
-          <img className="h-25 w-25" src={logo} alt="logo" />
-          <span className="-mt-5 cursor-default text-xl">Security Guide</span>
+          <img
+            className="h-25 w-25 animate-[bounce_1s_ease-in-out_infinite]"
+            src={logo}
+            alt="logo"
+          />
+          <span className="text-muted-foreground -mt-5 cursor-default text-xl">
+            <span className="text-primary mr-1">Security</span>
+            Guide
+          </span>
         </div>
       </div>
     </div>
